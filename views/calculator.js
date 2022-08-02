95dcaddb7e4e939d1cdb83f2286624613cc52066
Moza <ahmadzaei@hotmail.de>
2022-08-02 19:51:54 +0200

    Test Commit

--- views/calculator.js
+++ views/calculator.js
@@ -2,22 +2,22 @@ let display = document.getElementById('display');
 
 let buttons = Array.from(document.getElementsByClassName('button'));
 
-buttons.map( button => {
+buttons.map(button => {
     button.addEventListener('click', (e) => {
-        switch(e.target.innerText){
+        switch (e.target.innerText) {
             case 'C':
                 display.innerText = '';
                 break;
             case '=':
-                try{
+                try {
                     display.innerText = eval(display.innerText);
                 } catch {
-                    display.innerText = "Error"
+                    display.innerText = 'Error';
                 }
                 break;
             case '←':
-                if (display.innerText){
-                   display.innerText = display.innerText.slice(0, -1);
+                if (display.innerText) {
+                    display.innerText = display.innerText.slice(0, -1);
                 }
                 break;
             default:
